f549625d7c873e618be125aac793ce5498f3f52f
lxdevm <lcolindev@gmail.com>
2017-07-18 16:56:13 +0200

    structure the project

---
+++ server.js
@@ -0,0 +1,18 @@
+var apiai = require('apiai');
+var products = require('./key-data/product.json');
+
+var app = apiai("bf9c7e62eeb84ca28a387a6f9d48b76d");
+console.log(products[0].SuplyName);
+var request = app.textRequest('I have a dogs', {
+    sessionId: '1'
+});
+
+request.on('response', function(response) {
+    console.log(response);
+});
+
+request.on('error', function(error) {
+    console.log(error);
+});
+
+request.end();
